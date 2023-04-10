06b0491ed9641b5c6727c144454699a0313c4d00
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-04-10 13:36:04 +0600

    Folder Structure Created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <div>
-            <h3>Header Components</h3>
             <Link to="">Home</Link>
             <Link to="/statistics">Statistics</Link>
             <Link to="/appliedjobs">Applied Jobs</Link>
