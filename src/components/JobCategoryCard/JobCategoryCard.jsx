d1fe401dad3968124407e41a252d676bfbf44686
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-04-11 00:31:22 +0600

    Responsive added to Job category

--- src/components/JobCategoryCard/JobCategoryCard.jsx
+++ src/components/JobCategoryCard/JobCategoryCard.jsx
@@ -3,9 +3,9 @@ import Account from '../../assets/Icons/accounts 1.png'
 const JobCategoryCard = ({ category }) => {
     const { jobCategoryName, totalJobAvailable, imageLink } = category;
     return (
-        <div className='border-2 p-4 leading-normal'>
+        <div className='border-2 p-4 '>
             <img src={imageLink} alt='' />
-            <h3>{jobCategoryName}</h3>
+            <h3 className='font-bold my-2'>{jobCategoryName}</h3>
             <p>{totalJobAvailable}+ Jobs Available</p>
         </div>
     );
