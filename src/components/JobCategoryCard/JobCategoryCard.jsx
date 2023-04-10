c169117504023ce93011bf987f609d5467efb46f
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-04-11 00:32:36 +0600

    Update JobCategoryCard.jsx

--- src/components/JobCategoryCard/JobCategoryCard.jsx
+++ src/components/JobCategoryCard/JobCategoryCard.jsx
@@ -6,7 +6,7 @@ const JobCategoryCard = ({ category }) => {
         <div className='border-2 p-4 '>
             <img src={imageLink} alt='' />
             <h3 className='font-bold my-2'>{jobCategoryName}</h3>
-            <p>{totalJobAvailable}+ Jobs Available</p>
+            <p>{totalJobAvailable} Jobs Available</p>
         </div>
     );
 }
