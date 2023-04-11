a654241f26c83cca1539c4a446cbe29c023fc81b
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-04-11 10:03:19 +0600

    Footer and json data updated

--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from '../Header/Header';
 import { Outlet } from 'react-router-dom';
 import './Main.css';
+import Footer from '../Footer/Footer';
 
 const Main = () => {
     return (
         <div className='Main'>
             <Header />
             <Outlet />
+            <Footer/>
         </div>
     );
 }
