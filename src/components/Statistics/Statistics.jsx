dd61681ab70da4b6c15d568a1b464860abcf690c
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-04-11 11:26:10 +0600

    Chart Added for Assignment Marks (Area Chart)

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,65 @@
 import React from 'react';
-
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 const Statistics = () => {
+    const data = [
+        {
+            name: "A1",
+            marks: 60,
+        },
+        {
+            name: "A2",
+            marks: 57,
+        },
+        {
+            name: "A3",
+            marks: 60,
+        },
+        {
+            name: "A4",
+            marks: 55,
+        },
+        {
+            name: "A5",
+            marks: 56,
+        },
+        {
+            name: "A6",
+            marks: 53,
+        },
+        {
+            name: "A7",
+            marks: 60,
+        },
+        {
+            name: "A8",
+            marks: 60,
+        },
+    ];
+
     return (
-        <div>
-            <h3>From Statistics</h3>
+        <div className='p-3 lg:px-16'>
+            <h3 className='text-center font-semibold text-2xl py-5'>All of My Assignment Marks (A1-A8)</h3>
+            <hr className='mb-5' />
+            <ResponsiveContainer width='100%' height={200}>
+                <AreaChart
+                    width={500}
+                    height={200}
+                    data={data}
+                    syncId="anyId"
+                    margin={{
+                        top: 10,
+                        right: 30,
+                        left: 0,
+                        bottom: 0,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Area type="monotone" dataKey="marks" stroke="#82ca9d" fill="#82ca9d" />
+                </AreaChart>
+            </ResponsiveContainer>
         </div>
     );
 }
